07043bb2af53321eeb5feea3f60033c45522511c
Tristin Smith <tsmithassoc@gmail.com>
2024-04-25 20:54:02 -0400

    Initial Commit

---
+++ iConvert/iConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConvertApp.swift
+//  iConvert
+//
+//  Created by Tristin Smith on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct iConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
